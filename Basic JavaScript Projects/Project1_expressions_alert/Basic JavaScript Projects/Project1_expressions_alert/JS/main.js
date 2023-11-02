7991a04d1a978f03a9a47b3f510cd05edaa5ae35
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2023-11-01 20:42:17 -0400

    Project2_functions
    It include in/Basic JavaScript Project

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+/*var mom="NKundabera", First_born='stanley', Last='Faustin';
+document.write(Last);
+alert(3+6);
+*/
+
+/*function My_First_Function() { //Defining a function and naming it
+    var str = "This is the button text !";//Defining a variable and giving it  a string value
+    document.getElementById("button_text").innerHTML=str;//Putting the value of the variable into the
+                                                        //HTML element from point eith the "btton_text" as id
+}*/
+
+window.alert('This is for window alert'); //this is for window alert
+document.write('This is for document.alert'); // this is for document.write
+
+var Sent1 ="This is the beginning of the string";
+
+var Sent2 ="  and this is the end of the string";
+
+alert(Sent1 + Sent2);
---
+++ Basic JavaScript Projects/Project1_expressions_alert/Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function Two_variable(){
+    var name='Gashema';
+    var ag=' has 24';
+    var g= ' and half';
+    name+=ag+=g;//here I concatinate all declared variables 
+
+    document.getElementById("button_name").innerHTML=name;
+    //document.getElementById("button_name").innerHTML=age;
+}
